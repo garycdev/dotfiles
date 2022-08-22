00908534f54004b820231e0f12fb3ecbb5a60dc4
gcoddev <geco.yak77@gmail.com>
2022-08-22 11:51:38 -0400

    dotfiles Gary/ArchLinux

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Fondo de pantalla
+feh --bg-fill /usr/share/backgrounds/Yakys.jpg &
+# Iconos del sistema
+notify-send "Bienvenido de nuevo Gary" &
+udiskie -t &
+nm-applet &
+volumeicon &
+cbatticon -u 5 &
+picom --config ~/.config/picom/picom.conf  &
+alacritty &
